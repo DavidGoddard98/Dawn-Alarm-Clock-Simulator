708863ec7db1a201b5018fad3f97fb66f76bd997
Theo Koorehpaz <tkoorehpaz1@sheffield.ac.uk>
2019-12-18 23:25:10 +0000

    Fixed IR Sensor

--- MyProjectThing/main/main.cpp
+++ MyProjectThing/main/main.cpp
@@ -73,11 +73,8 @@ int rc;
 int pixelBrightness = 0;
 
 // IR SENSOR stuff
-#define PIR_AOUT A1  // PIR analog output on A0
 #define PIR_DOUT 27   // PIR digital output on D2
 #define LED_PIN  13  // LED to illuminate on motion
-#define PRINT_TIME 100 // Rate of serial printouts
-unsigned long lastPrint = 0; // Keep track of last serial out
 
 /////////////////////////////////////////
 
@@ -160,11 +157,11 @@ void setup() {
   getMAC(MAC_ADDRESS);          // store the MAC address
   apSSID.concat(MAC_ADDRESS);   // add the MAC to the AP SSID
 
-
-  digitalRead(LED_PIN);
-  pinMode(PIR_AOUT, INPUT);
   pinMode(PIR_DOUT, INPUT);
+  // Configure the motion indicator LED pin as an output
   pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW); // Turn the LED off
+
   //Show Wifi UI
   uiCont = new UIController(ui_boot);
   if(!uiCont->begin()) {
@@ -207,6 +204,8 @@ void loop() {
   unsigned long int timer_2 = micros();
   long int time_iter = 0;
   while(1) {
+
+    // readDigitalValue();
 
     //only needs to be run every second
     if (loopIter % 80 == 0) {
@@ -367,10 +366,11 @@ void forcedSleep() {
 // IR SENSOR METHODS
 void readDigitalValue()
 {
+  delay(1000);
   // The OpenPIR's digital output is active high
   int motionStatus = digitalRead(PIR_DOUT);
-  Serial.print(motionStatus );
-  Serial.println("here ");
+  Serial.print("motion status: ");
+  Serial.println(motionStatus);
 
   // If motion is detected, turn the onboard LED on:
   if (motionStatus == HIGH) {
@@ -380,30 +380,6 @@ void readDigitalValue()
   else {
     digitalWrite(LED_PIN, LOW);
     Serial.println("no motion");
-  }
-
-}
-
-void printAnalogValue()
-{
-  if ( (lastPrint + PRINT_TIME) < millis() )
-  {
-    lastPrint = millis();
-    // Read in analog value:
-    unsigned int analogPIR = analogRead(PIR_AOUT);
-    // Convert 10-bit analog value to a voltage
-    // (Assume high voltage is 5.0V.)
-    float voltage = (float) analogPIR / 1024.0 * 5.0;
-    // Print the reading from the digital pin.
-    // Mutliply by 5 to maintain scale with AOUT.
-    Serial.print(5 * digitalRead(PIR_DOUT));
-    Serial.print(',');    // Print a comma
-    Serial.print(2.5);    // Print the upper limit
-    Serial.print(',');    // Print a comma
-    Serial.print(1.7);    // Print the lower limit
-    Serial.print(',');    // Print a comma
-    Serial.print(voltage); // Print voltage
-    Serial.println();
   }
 }
 
