5d7e43b5a28a53eb4e5f1c4fc26ef2f6b679312b
David goddard <dgoddard3@sheffield.ac.uk>
2020-01-25 17:05:17 +0000

    Fixed provisioning

--- MyProjectThing/main/main.cpp
+++ MyProjectThing/main/main.cpp
@@ -9,30 +9,10 @@
 
 //***********************************************************************
 // a library or two... ///////////////////////////////////////////////////////
-#include <stdio.h>
-#include <tuple>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <esp_system.h>
-#include <esp_spi_flash.h>
-#include <Arduino.h>
-#include <SPI.h>
-#include <Wire.h>
-#include <WiFi.h>
 #include "private.h" // stuff not for checking in
 #include "unphone.h"
 #include "IOExpander.h"
 #include "UIController.h"
-#include "joinme.h"
-#include <FS.h>
-#include <SPIFFS.h>
-#include <ArduinoJson.h>
-
-//time
-#include "time.h"
-
-//power consumption
-#include "driver/rtc_io.h"
 
 //TIME FUNCTIONS
 #include <soc/rtc.h>
@@ -60,7 +40,7 @@ const int   daylightOffset_sec = 0;
 UIController *uiCont;
 #define uS_TO_S_FACTOR 1000000  //Convert seconds to microseconds
 // globals for a wifi access point and webserver ////////////////////////////
-String apSSID = String("Paz_Dave_Unphone-"); // SSID of the AP
+String apSSID = String("Paz_Dave_Alarm-"); // SSID of the AP
 String apPassword = _DEFAULT_AP_KEY;     // passkey for the AP
 uint64_t timeDiff, timeNow; //RTC clock variables
 
@@ -70,7 +50,7 @@ uint64_t timeDiff, timeNow; //RTC clock variables
 #include	<string.h>
 #include	"esp_event_loop.h"	//	for usleep
 #include	"neopixel.h"
-#include "neopixel.c"
+#include  "neopixel.c"
 #include	<esp_log.h>
 
 //constants and instants
@@ -86,31 +66,6 @@ float pixBr = 0;
 #define PIR_DOUT 27   // PIR digital output on D2
 #define LED_PIN  13  // LED to illuminate on motion
 
-////////////////////////////////////////////////////////////////////////////////
-
-//PROVISIONING//////////////////////////////////////////////////////////////////
-#include <ESPAsyncWebServer.h>
-
-//instants
-AsyncWebServer* webServer;               // async web server
-typedef struct { int position; const char *replacement; } replacement_t;
-void getHtml(String& html, const char *[], int, replacement_t [], int);
-
-//methods
-void getHtml( String& html, const char *boiler[], int boilerLen,replacement_t repls[], int replsLen);
-void initWebServer();
-void hndlRoot(AsyncWebServerRequest *);
-void hndlNotFound(AsyncWebServerRequest *);
-void hndlWifichz(AsyncWebServerRequest *);
-void hndlStatus(AsyncWebServerRequest *);
-void hndlWifi(AsyncWebServerRequest *);
-void apListForm(String&);
-void printIPs();
-String ip2str(IPAddress address);
-void getWiFiNetworks();
-#define ALEN(a) ((int) (sizeof(a) / sizeof(a[0]))) // only in definition scope!
-#define GET_HTML(strout, boiler, repls) \
-getHtml(strout, boiler, ALEN(boiler), repls, ALEN(repls));
 ////////////////////////////////////////////////////////////////////////////////
 
 //SAVE DATA OVER BOOT IN RCU MEMORY/////////////////////////////////////////////
@@ -231,6 +186,10 @@ void loop() {
     }
 
 
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.print("Not connected!!");
+    }
+
 
     //If power switch not on turn unphone off (see powerMode function)
     if (!powerOn()) powerMode();
@@ -311,18 +270,10 @@ void fetchTime() {
   uiCont->showUI(ui_WiFi); //show connecting to wifi UI
 
   //Connect to saved wifi, if none start AP
-  WiFi.begin();
-  delay(3000);
-  Serial.printf("Starting AP");
-  WiFi.mode(WIFI_STA);
-  //create access point and set up its credentials
-  WiFi.softAP(apSSID.c_str(), apPassword.c_str());
-  vTaskDelay(400); //settle
-
-  // init the web server
-  // webServer = new AsyncWebServer(80);
-  // initWebServer();
-  vTaskDelay(400); //settle
+  Serial.printf("doing wifi manager\n");
+  joinmeManageWiFi(apSSID.c_str(), apPassword.c_str());
+  Serial.printf("wifi manager done\n\n");
+  delay(1000); //let wifi settle
 
   //let wifi AP settle
   if (WiFi.status() != WL_CONNECTED) {
@@ -656,205 +607,4 @@ char *getMAC(char *buf) { // the MAC is 6 bytes; needs careful conversion...
   }
   buf[12] = '\0';
   return buf;
-}
-// web server utils //////////////////////////////////////////////////////////
-void getHtml( // turn array of strings & set of replacements into a String
-  String& html, const char *boiler[], int boilerLen,
-  replacement_t repls[], int replsLen
-) {
-  for(int i = 0, j = 0; i < boilerLen; i++) {
-    if(j < replsLen && repls[j].position == i)
-      html.concat(repls[j++].replacement);
-    else
-      html.concat(boiler[i]);
-  }
-}
-
-const char *templatePage[] = {    // we'll use Ex07 templating to build pages
-  "<html><head><title>",                                                //  0
-  "default title",                                                      //  1
-  "</title>\n",                                                         //  2
-  "<meta charset='utf-8'>",                                             //  3
-  "<meta name='viewport' content='width=device-width, initial-scale=1.0'>\n"
-  "<style>body{background:#FFF; color: #000; font-family: sans-serif;", //  4
-  "font-size: 150%;}</style>\n",                                        //  5
-  "</head><body>\n",                                                    //  6
-  "<h2>Welcome to Thing!</h2>\n",                                       //  7
-  "<!-- page payload goes here... -->\n",                               //  8
-  "<!-- ...and/or here... -->\n",                                       //  9
-  "\n<p><a href='/'>Home</a>&nbsp;&nbsp;&nbsp;</p>\n",                  // 10
-  "</body></html>\n\n",                                                 // 11
-};
-void initWebServer() { // changed naming conventions to avoid clash with Ex06
-  // register callbacks to handle different paths
-  webServer->on("/", hndlRoot);              // slash
-  webServer->onNotFound(hndlNotFound);       // 404s...
-  webServer->on("/generate_204", hndlRoot);  // Android captive portal support
-  webServer->on("/L0", hndlRoot);            // erm, is this...
-  webServer->on("/L2", hndlRoot);            // ...IoS captive portal...
-  webServer->on("/ALL", hndlRoot);           // ...stuff?
-  webServer->on("/wifi", hndlWifi);          // page for choosing an AP
-  webServer->on("/wifichz", hndlWifichz);    // landing page for AP form submit
-  webServer->on("/status", hndlStatus);      // status check, e.g. IP address
-
-  webServer->begin();
-  dln(startupDBG, "HTTP server started");
-}
-
-// webserver handler callbacks
-void hndlNotFound(AsyncWebServerRequest *request) {
-  dbg(netDBG, "URI Not Found: ");
-  dln(netDBG, request->url());
-  request->send(200, "text/plain", "URI Not Found");
-}
-void hndlRoot(AsyncWebServerRequest *request) {
-  dln(netDBG, "serving page notionally at /");
-  replacement_t repls[] = { // the elements to replace in the boilerplate
-    {  1, apSSID.c_str() },
-    {  8, "" },
-    {  9, "<p>Choose a <a href=\"wifi\">wifi access point</a>.</p>" },
-    { 10, "<p>Check <a href='/status'>wifi status</a>.</p>" },
-  };
-  String htmlPage = ""; // a String to hold the resultant page
-  GET_HTML(htmlPage, templatePage, repls); // GET_HTML sneakily added to Ex07
-  request->send(200, "text/html", htmlPage);
-}
-void hndlWifi(AsyncWebServerRequest *request) {
-  dln(netDBG, "serving page at /wifi");
-
-  String form = ""; // a form for choosing an access point and entering key
-  apListForm(form);
-  replacement_t repls[] = { // the elements to replace in the boilerplate
-    { 1, apSSID.c_str() },
-    { 7, "<h2>Network configuration</h2>\n" },
-    { 8, "" },
-    { 9, form.c_str() },
-  };
-  String htmlPage = ""; // a String to hold the resultant page
-  GET_HTML(htmlPage, templatePage, repls); // GET_HTML sneakily added to Ex07
-
-  request->send(200, "text/html", htmlPage);
-}
-void hndlWifichz(AsyncWebServerRequest *request) {
-  dln(netDBG, "serving page at /wifichz");
-
-  String title = "<h2>Joining wifi network...</h2>";
-  String message = "<p>Check <a href='/status'>wifi status</a>.</p>";
-
-  String ssid = "";
-  String key = "";
-  for(uint8_t i = 0; i < request->args(); i++ ) {
-    if(request->argName(i) == "ssid")
-      ssid = request->arg(i);
-    else if(request->argName(i) == "key")
-      key = request->arg(i);
-  }
-
-  if(ssid == "") {
-    message = "<h2>Ooops, no SSID...?</h2>\n<p>Looks like a bug :-(</p>";
-  } else {
-    char ssidchars[ssid.length()+1];
-    char keychars[key.length()+1];
-    ssid.toCharArray(ssidchars, ssid.length()+1);
-    key.toCharArray(keychars, key.length()+1);
-    WiFi.begin(ssidchars, keychars);
-  }
-
-  replacement_t repls[] = { // the elements to replace in the template
-    { 1, apSSID.c_str() },
-    { 7, title.c_str() },
-    { 8, "" },
-    { 9, message.c_str() },
-  };
-  String htmlPage = "";     // a String to hold the resultant page
-  GET_HTML(htmlPage, templatePage, repls);
-
-  request->send(200, "text/html", htmlPage);
-}
-void hndlStatus(AsyncWebServerRequest *request) { // UI for checking connectivity etc.
-  dln(netDBG, "serving page at /status");
-
-  String s = "";
-  s += "<ul>\n";
-  s += "\n<li>SSID: ";
-  s += WiFi.SSID();
-  s += "</li>";
-  s += "\n<li>Status: ";
-  switch(WiFi.status()) {
-    case WL_IDLE_STATUS:
-      s += "WL_IDLE_STATUS</li>"; break;
-    case WL_NO_SSID_AVAIL:
-      s += "WL_NO_SSID_AVAIL</li>"; break;
-    case WL_SCAN_COMPLETED:
-      s += "WL_SCAN_COMPLETED</li>"; break;
-    case WL_CONNECTED:
-      s += "WL_CONNECTED</li>"; break;
-    case WL_CONNECT_FAILED:
-      s += "WL_CONNECT_FAILED</li>"; break;
-    case WL_CONNECTION_LOST:
-      s += "WL_CONNECTION_LOST</li>"; break;
-    case WL_DISCONNECTED:
-      s += "WL_DISCONNECTED</li>"; break;
-    default:
-      s += "unknown</li>";
-  }
-
-  s += "\n<li>Local IP: ";     s += ip2str(WiFi.localIP());
-  s += "</li>\n";
-  s += "\n<li>Soft AP IP: ";   s += ip2str(WiFi.softAPIP());
-  s += "</li>\n";
-  s += "\n<li>AP SSID name: "; s += apSSID;
-  s += "</li>\n";
-
-  s += "</ul></p>";
-
-  replacement_t repls[] = { // the elements to replace in the boilerplate
-    { 1, apSSID.c_str() },
-    { 7, "<h2>Status</h2>\n" },
-    { 8, "" },
-    { 9, s.c_str() },
-  };
-  String htmlPage = ""; // a String to hold the resultant page
-  GET_HTML(htmlPage, templatePage, repls); // GET_HTML sneakily added to Ex07
-
-  request->send(200, "text/html", htmlPage);
-}
-void apListForm(String& f) { // utility to create a form for choosing AP
-  const char *checked = " checked";
-  int n = WiFi.scanNetworks();
-  dbg(netDBG, "scan done: ");
-
-  if(n == 0) {
-    dln(netDBG, "no networks found");
-    f += "No wifi access points found :-( ";
-    f += "<a href='/'>Back</a><br/><a href='/wifi'>Try again?</a></p>\n";
-  } else {
-    dbg(netDBG, n); dln(netDBG, " networks found");
-    f += "<p>Wifi access points available:</p>\n"
-         "<p><form method='POST' action='wifichz'> ";
-    for(int i = 0; i < n; ++i) {
-      f.concat("<input type='radio' name='ssid' value='");
-      f.concat(WiFi.SSID(i));
-      f.concat("'");
-      f.concat(checked);
-      f.concat(">");
-      f.concat(WiFi.SSID(i));
-      f.concat(" (");
-      f.concat(WiFi.RSSI(i));
-      f.concat(" dBm)");
-      f.concat("<br/>\n");
-      checked = "";
-    }
-    f += "<br/> <br/>";
-    // custom option to choose uos other network
-    f += "UOS-other<input type='radio' name='ssid' value='uos-other' uos-other <br/><br/>";
-    f += "<br/>Pass key: <input type='textarea' name='key'><br/><br/> ";
-    f += "<input type='submit' value='Submit'></form></p>";
-
-  }
-}
-String ip2str(IPAddress address) { // utility for printing IP addresses
-  return
-    String(address[0]) + "." + String(address[1]) + "." +
-    String(address[2]) + "." + String(address[3]);
 }
